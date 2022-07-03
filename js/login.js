1bd04ff4d8e13c2f5346bbba7d008f57eb6cc5a1
Joacom7 <joacom7@gmail.com>
2022-07-03 17:57:23 -0300

    feat: login form

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const loginButtonElements = document.getElementsByClassName('login-button')
+const loginButton = loginButtonElements[0]
+const BlockAccountButton = loginButtonElements[1]
+     
+console.log('login-buton-elements', loginButtonElements)
+console.log('login-buton', loginButton)
+
+
+
+ 
